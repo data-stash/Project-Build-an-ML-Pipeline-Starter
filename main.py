fab39858f15ef3134060fe9b3129894e2d3cd02f
data-stash <data-stash@proton.me>
2026-03-18 16:24:21 -0400

    spelling

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def go(config: DictConfig):
         if "data_split" in active_steps:
             
             _ = mlflow.run(
-                f"{config['main']['components_reporitory']}/train_val_test_split",
+                f"{config['main']['components_repository']}/train_val_test_split",
                 "main",
                 parameters={
                     "input": "nyc_airbnb/clean_sample.csv:latest",
